4a573a59ce0070b35b2024db45a00ee9d2eafbf2
ikaliuzh <ikaliuzh@gmail.com>
2021-11-02 11:27:02 +0200

    added matrixes

--- source/Matrix.hpp
+++ source/Matrix.hpp
@@ -8,21 +8,25 @@
 template <typename T, size_t M, size_t N>
 class Matrix {
 public:
-	Matrix() : data(M, std::vector<T>(N)) {};
-	Matrix(std::initializer_list<std::initializer_list<T>> _data) {
+	Matrix() : data(M, std::vector<T>(N, 0)) {};
+
+	Matrix(std::initializer_list<std::initializer_list<T>> _data)
+	 : data(M, std::vector<T>(N)) {
 		assert(M == _data.size());
 		size_t i = 0;
-		for (const auto& v : _data){
-			if (i == 0){
-				assert(N == v.size());
+		for (const auto& row : _data){
+			assert(N == row.size());
+			size_t j = 0;
+			for (const auto& element : row){
+				data[i][j] = element;
+				++j;
 			}
-			data[i] = std::vector<T>(v);
 			++i;
 		}
 	};
 
-	std::vector<T>& operator[](size_t i) { return data[i]; }
-	const std::vector<T>& operator[](size_t i) const { return data[i]; }
+	std::vector<T>& operator[](const size_t i) { return data[i]; }
+	const std::vector<T>& operator[](const size_t i) const { return data[i]; }
 
 	std::pair<size_t, size_t> size() const { return {data.size(), data[0].size()}; }
 private:
@@ -48,4 +52,27 @@ Matrix<T, M, N> operator+(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs) {
 		}
 	}
 	return lhs;
+}
+
+template <typename T, size_t M, size_t N>
+Matrix<T, M, N> operator-(Matrix<T, M, N> lhs, const Matrix<T, M, N>& rhs) {
+	for (size_t i = 0; i < M; ++i) {
+		for (size_t j = 0; j < N; ++i) {
+			lhs[i][j] -= rhs[i][j];
+		}
+	}
+	return lhs;
+}
+
+template <typename T, size_t M1, size_t M2, size_t M3>
+Matrix<T, M1, M3> operator*(Matrix<T, M1, M2> lhs, const Matrix<T, M2, M3>& rhs) {
+	Matrix<T, M1, M3> result;
+	for (size_t i = 0; i < M1; ++i) {
+		for (size_t j = 0; j < M3; ++i) {
+			for (size_t k = 0; k < M2; ++k){
+				result[i][j] += lhs[i][k] * rhs[k][j];
+			}
+		}
+	}
+	return result;
 }
--- source/lab1.cpp
+++ source/lab1.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]){
 	/* Nodes::Uniform nodes{20, -4, 4};
 	CubicSpline cs(&nodes);
 	cs.report(); */
-	Matrix<double, 2, 2> A{{1, 2}, {3, 4}}, B{{1, 2}, {0 , 0}};
+	Matrix<double, 2, 2> A{{1, 2}, {3, 4}}, B{{1, 2}, {0, 0}};
 	std::cout << A;
 	return 0;
 }
