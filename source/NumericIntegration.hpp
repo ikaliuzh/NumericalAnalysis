6e0384377e6080de290c821c8a2a83e966080857
ikaliuzh <ikaliuzh@gmail.com>
2021-12-16 20:59:27 +0200

    bug fixes

--- source/NumericIntegration.hpp
+++ source/NumericIntegration.hpp
@@ -4,13 +4,39 @@
 #include <cmath>
 
 
-long double __integrate(
+long double __integrate_trapezoidal(
 		long double (*f)(long double),
 		long double a,
 		long double b,
 		size_t n){
 	long double h = (b - a) / n;
-	long double result = 0;
+	long double c = a, d = b;
+
+	while (std::isnan(f(c)) && c < a + h) {
+		c += h / 100.;
+	}
+	while (std::isnan(f(d)) && d > b - h) {
+		d -= h / 100.;
+	}
+	long double result = (f(c) + f(d))/2.;
+	// h = (d - c) / n;
+
+	for (size_t i = 1; i < n; ++i) {
+		long double xi = a + h * i;
+		// while (std::isnan(f(xi)) && xi < a + h * i + h) {
+		// 	xi += h / 100.;
+		// }
+		result += f(xi);
+	}
+	return result * h;
+}
+
+long double __integrate_composite_simpson(
+		long double (*f)(long double),
+		long double a,
+		long double b,
+		size_t n){
+	long double h = (b - a) / n;
 	long double c = a, d = b;
 
 	while (std::isnan(f(c)) && c < a + h) {
@@ -19,16 +45,23 @@ long double __integrate(
 	while (std::isnan(f(d)) && d > b - h) {
 		c -= h / 100.;
 	}
-	h = (d - c) / n;
+	// h = (d - c) / n;
+
+
+	long double sum_odds = 0, sum_evens = 0; 	
 
 	for (size_t i = 1; i < n; ++i) {
-		long double xi = c + h * i;
-		while (std::isnan(f(xi)) && xi < c + h * i + h) {
-			xi += h / 20.;
-		}
-		result += f(xi);
+		long double xi = a + h * i;
+		// while (std::isnan(f(xi)) && xi < c + h * i + h) {
+		// 	xi += h / 100.;
+		// }
+		if (i % 2)
+			sum_odds += f(xi);
+		else
+			sum_evens += f(xi);
 	}
-	return result * h;
+
+	return (f(c) + f(d) + 2 * sum_evens + 4 * sum_odds) * h / 3;
 }
 
 
@@ -36,20 +69,35 @@ std::optional<long double> integrate(
 		long double (*f)(long double),
 		long double a,
 		long double b,
-		long double eps){
+		long double eps,
+		char method){
+	int p;
+	long double (*integrator)(
+		long double (*f)(long double),
+		long double a,
+		long double b,
+		size_t n);
 
-	int p = 2;
+	if (method == 's'){
+		p = 2;
+		integrator = __integrate_composite_simpson;
+	} else if (method == 't'){
+		p = 1;
+		integrator = __integrate_trapezoidal;
+	}
+	
 	long double k = 1. / (pow(2, p) - 1);
 	int n = 1;
 	while (true) {
-		long double In = __integrate(f, a, b, n);
-		long double I2n = __integrate(f, a, b, 2 * n);
+		long double In = integrator(f, a, b, n);
+		long double I2n = integrator(f, a, b, 2 * n);
 		if (k * abs(In - I2n) < eps) {
+			std::cout << n << std::endl;
 			return I2n;
 		}
 		if (n > 100'000){
 			return {};
 		}
-		n += 200;
+		n *= 2;
 	}
 }
--- source/main.cpp
+++ source/main.cpp
@@ -8,23 +8,6 @@ long double g (long double x) {
 }
 
 int main(int argc, char *argv[]){
-//	Nodes::Uniform nodes{40, -4, 4};
-//	CubicSpline cs(&nodes);
-//	cs.report();
-
-//	SquareMatrix<4> A{{2, 4, -1, -2}, {6, 1, 3, 4}, {-7, 1, 1, -1}, {0, 2, -3, 5}};
-//	Vector<4> b{{1}, {1}, {3}, {5}};
-//
-//	LinearSystemSolver lss(A, b);
-//	auto solution = lss.solve(1e-7);
-//	if (solution.has_value()) {
-//		std::cout << "Решение:\n" << solution.value() << "Невязка:\n" << b - A * solution.value();
-//	} else {
-//		std::cout << "Failed to solve!";
-//	}
-//	return 0;
-
-	std::cout << integrate(g, 0, pi/2, 1e-5).value();
-	// std::cout << __integrate(g, 0, pi/2, 10000);
+	std::cout << integrate(g, 0, pi/2, 1e-5, 't').value() << std::endl;
 }
 
